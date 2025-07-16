1388f17420ff2d6ea2810d7082fa804ad6480969
Croximity <106570269+Croximity@users.noreply.github.com>
2025-07-16 16:36:34 -0600

    prueba

--- backend/rutas/seguridad/authRoutes.js
+++ backend/rutas/seguridad/authRoutes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const { check } = require('express-validator');
 const router = express.Router();
 const authController = require('../../controladores/seguridad/authController');
-
+//prueba
 // Ruta: Registro de usuario
 router.post(
   '/registro',
