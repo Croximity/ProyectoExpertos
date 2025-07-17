d2460a8481cc822d1752cde4b7abf21d7625d663
croximity <croximity@gmail.com>
2025-07-16 21:43:14 -0600

    Correccion y optimizacion del app.js

--- backend/app.js
+++ backend/app.js
@@ -124,13 +124,14 @@ const FacturaDetalle = require('./modelos/facturacion/FacturaDetalle');
 const FormaPago = require('./modelos/facturacion/FormaPago');
 const Descuento = require('./modelos/facturacion/Descuento');
 const DetalleDescuento = require('./modelos/facturacion/DetalleDescuento');
+const Atributo = require('./modelos/productos/Atributo');
 
-// También podés sincronizar modelos de productos si querés individualmente:
-// const Producto = require('./modelos/productos/ProductoModel');
-// const CategoriaProducto = require('./modelos/productos/CategoriaProducto');
-// etc.
+//modelos de productos
+const Producto = require('./modelos/productos/ProductoModel');
+const CategoriaProducto = require('./modelos/productos/CategoriaProducto');
+const ProductoAtributo = require('./modelos/productos/ProductoAtributo');
 
-// Iniciar servidor y sincronizar base de datos
+
 const startServer = async () => {
   try {
     await db.authenticate();
@@ -157,8 +158,11 @@ const startServer = async () => {
     console.log('✅ Modelos de consulta exámenes sincronizados.');
 
     // Sincronizar el resto (productos, etc.)
-    await db.sync({ alter: true });
-    console.log('🔄 Modelos de productos/inventario sincronizados.');
+    await Atributo.sync();
+    await Producto.sync();
+    await CategoriaProducto.sync();
+    await ProductoAtributo.sync();
+    console.log('✅ Modelos de productos/inventario sincronizados.');
 
         // Sincronizar modelos de Fcaturacion
     await Descuento.sync();
