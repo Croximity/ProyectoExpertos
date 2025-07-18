b6b09137744a7202859b951c0ec0033c8538956e
croximity <croximity@gmail.com>
2025-07-17 20:10:18 -0600

    Correcion Swagger buena

--- backend/rutas/consulta_examenes/DiagnosticoRuta.js
+++ backend/rutas/consulta_examenes/DiagnosticoRuta.js
@@ -58,7 +58,7 @@ const { verificarUsuario } = require('../../configuraciones/passport');
  *             $ref: '#/components/schemas/DiagnosticoInput'
  *           example:
  *             idConsulta: 10
- *             idTipoEnfermedad:1
+ *             idTipoEnfermedad: 1
  *             idExamen: 1
  *             descripcion: "Miopía leve detectada"
  *             fecha: "2025-07-17"
